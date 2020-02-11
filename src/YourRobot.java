48571edf4557bcddfdd66620fcbf51877157101b
Angela Barros <angelabarros29@hotmail.com>
2020-02-11 19:12:49 +0000

    First Commit

---
+++ src/YourRobot.java
@@ -0,0 +1,28 @@
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import robocode.AdvancedRobot;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+
+public class YourRobot extends AdvancedRobot {
+
+	public void run() {
+		while(true) {
+			ahead(100);
+			turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+            
+            setAdjustGunForRobotTurn(true);
+            setAdjustRadarForGunTurn(true);
+		}
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e) {
+        //fire(1);
+    }
+	
+	
+}
